67c7007b37abf79e9e4212197e7c9f16c1e5355d
yuki@home <tomita_y@clowncolor.com>
2024-08-09 22:24:43 +0900

    add uploader

---
+++ src/utils/hf_uploader.py
@@ -0,0 +1,24 @@
+import argparse
+
+from huggingface_hub import HfApi, login
+
+
+def main(quantize:str):
+    login(token="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", add_to_git_credential=True)
+
+    api = HfApi()
+
+    for i in range(5):
+        api.upload_file(
+            path_or_fileobj=f"./data/model_responses/{quantize}/responses_{i}.json",
+            path_in_repo=f"/non-quantize/model_responses/{quantize}/responses_{i}.json",
+            repo_id="YukiTomita-CC/temp_data",
+            repo_type="dataset",
+        )
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run the app on a specified port.')
+    parser.add_argument('--quantize', type=str, help='quantize')
+    args = parser.parse_args()
+
+    main(quantize=args.quantize)
